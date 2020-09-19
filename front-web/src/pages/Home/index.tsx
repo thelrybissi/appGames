84fbf8fdaebd6c68b8c114dc87c27828447ae8f6
thelrybissi <thelry.d.b@hotmail.com>
2020-09-19 16:59:30 -0300

    Home Page

---
+++ front-web/src/pages/Home/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ReactComponent as ArrowIcon } from '../../assets/seta.svg'
+import { ReactComponent as Gamer } from '../../assets/gamer.svg'
+import './styles.css'
+
+const Home = () => (
+    <div className="home-container">
+        <div className="home-text">
+            <h1 className="home-text-title">Quais jogos a galera gosta mais?</h1>
+            <h3 className="home-text-subtitle">Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!</h3>
+            <div className="home-actions">
+                <button className="home-btn">QUERO SABER QUAIS SÃO</button>
+                <div className="home-btn-icon">
+                    <ArrowIcon />
+                </div>
+            </div>
+        </div>
+        <Gamer className="home-image" />
+    </div>
+);
+
+export default Home;
